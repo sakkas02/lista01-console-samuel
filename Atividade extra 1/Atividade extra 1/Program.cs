451304069de29a8f84477e051fc1a4b7cdacf339
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 10:21:33 -0300

    fiz a atividade extra 1

---
+++ Atividade extra 1/Atividade extra 1/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Digite a sua quantia em real");
+double real = double.Parse(Console.ReadLine());
+Console.WriteLine($"A quantia que você pode comprar em dolar é de : {real / 3.45}");
