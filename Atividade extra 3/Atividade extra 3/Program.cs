3f3131ab302891dafb0d1c5d0fd77e61ae62a635
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 10:36:03 -0300

    fiz a atividade extra 3

---
+++ Atividade extra 3/Atividade extra 3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Escreva a icognita A");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Escreva a icognita B");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Escreva a icognita C");
+double c = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"O Valor de delta é de: {((b*b) * (-4 * a * c))}");
