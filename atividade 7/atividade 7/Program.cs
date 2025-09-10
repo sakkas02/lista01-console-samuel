246ddd38cf9ecc08677cf34b0c83e918dc7e4e99
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 08:16:54 -0300

    Foi realizado a atividade 7 da lista.

---
+++ atividade 7/atividade 7/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Digite um valor!");
+int num1;
+num1 = int.Parse(Console.ReadLine());
+if (num1 > 10)
+{
+    Console.WriteLine("O número digitado é maior que 10!");
+}
+
+else
+{
+    Console.WriteLine("O número digitado é menor que 10!");
+}
