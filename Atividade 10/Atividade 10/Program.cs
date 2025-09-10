1da287aa3366a0bb4993bc3c0758403e885c36a2
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 08:36:48 -0300

    fiz a atividade 10

---
+++ Atividade 10/Atividade 10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+double nota1;
+double nota2;
+double media;
+Console.WriteLine("Escreva a sua primeira nota");
+nota1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Escreva a sua segunda nota");
+nota2 = double.Parse(Console.ReadLine());
+media = (nota1 + nota2) / 2;
+
+if (media>6)
+{
+    Console.WriteLine($"Sua situação é de APROVADO, com a média {media}");
+}
+else
+{
+    Console.WriteLine($"Sua situação é de REPROVADO, com a média {media}");
+}
