21f45483fd0be135ff73fa762a245d93bd0c34f7
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 10:24:54 -0300

    fiz a atividade extra 2

---
+++ Atividade extra 2/Atividade extra 2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Digite a largura da parede (em metros): ");
+double largura = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite a altura da parede (em metros): ");
+double altura = Convert.ToDouble(Console.ReadLine());
+double area = largura * altura;
+double tinta = area / 2;
+Console.WriteLine($"\nA área da parede é {area} m².");
+Console.WriteLine($"Você precisará de {tinta} litros de tinta para pintá-la.");
