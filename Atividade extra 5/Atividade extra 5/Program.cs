dc409c5d71884145d5893527f9e79350732d15be
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 10:48:55 -0300

    fiz a atividade extra 5

---
+++ Atividade extra 5/Atividade extra 5/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Escreva o seu salário atual");
+float salario = float.Parse(Console.ReadLine());
+Console.WriteLine($"Seu salário depois de um aumento de 15% será de: R${salario * 1.15}");
