5486515c13617d5a7f7fff0812d1c4c16d96fe6a
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 09:18:54 -0300

    fiz a atividade 4

---
+++ Atividade 4/Atividade 4/Program.cs
@@ -0,0 +1,7 @@
+﻿int anos;
+int dias;
+Console.WriteLine("Digite quantos anos você tem:");
+anos = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite quantos dias atrás foi seu aniversário:");
+dias = int.Parse(Console.ReadLine());
+Console.WriteLine($"Você tem {anos * 365 + dias} de vida");
