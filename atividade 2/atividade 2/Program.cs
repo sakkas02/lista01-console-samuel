3bd7610a6e0fe583bb42aad164505afa55e514f5
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 09:14:05 -0300

    fiz a atividade 2

--- atividade 2/atividade 2/Program.cs
+++ atividade 2/atividade 2/Program.cs
@@ -1,10 +1,4 @@
-﻿int largura;
-int altura;
-
-Console.WriteLine("Escreva a lagura do retangulo"); 
-largura = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Escreva a alutra do retangulo");
-altura = int.Parse(Console.ReadLine());
-
-Console.WriteLine($"A área é igual {altura*largura}");    
+﻿int numero;
+Console.WriteLine("Digite um número:");
+numero = int.Parse(Console.ReadLine());
+Console.WriteLine($"O antecessor do número é: {numero - 1}");
