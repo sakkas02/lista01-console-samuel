bd427821ffb5ea7ce3a237074ba742efbeed4003
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 09:16:29 -0300

    fiz a atividade 3

---
+++ Atividade 3/Atividade 3/Program.cs
@@ -0,0 +1,8 @@
+﻿int largura;
+int altura;
+
+Console.WriteLine("Digite a largura:");
+largura = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a altura:");
+altura = int.Parse(Console.ReadLine());
+Console.WriteLine($"A area do retangulo é {altura*largura}");
