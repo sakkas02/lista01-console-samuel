df825d27de04f2db51a2a256f9a87d4e46d1ac94
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 09:31:57 -0300

    fiz a atividade 11

---
+++ Atividade 11/Atividade 11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Design;
+
+int ano1;
+int ano2;
+
+Console.WriteLine("Escreva o ano atual");
+ano1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva o ano que você nasceu!");
+ano2 = int.Parse(Console.ReadLine());
+
+if ((ano1 - ano2) >= 18)
+{
+    Console.WriteLine("Seu voto é OBRIGATORIO");
+}
+else if ((ano1 - ano2) >= 16)
+{
+    Console.WriteLine("Seu voto é FACULTATIVO");
+}
+else
+{
+    Console.WriteLine("Você NÃO PODE votar esse ano");
+}
