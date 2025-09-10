caffe9c605a86bd3ffddf41a77041b1170084a7a
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 09:35:30 -0300

    fiz a atividade 12

---
+++ Atividade 12/Atividade 12/Program.cs
@@ -0,0 +1,16 @@
+﻿int num1;
+int num2;
+
+Console.WriteLine("Escreva o primeiro numero");
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Escreva o segundo numero");
+num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("O primeiro numero é maior que o segundo");
+}
+else
+{
+    Console.WriteLine("O segundo numero é maior que o primeiro");
+}
