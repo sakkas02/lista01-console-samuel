5024975d89020d257c202df348afbaab0b2af45c
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 09:24:16 -0300

    fiz a atividade 6

---
+++ Atividade 6/Atividade 6/Program.cs
@@ -0,0 +1,7 @@
+﻿float salarioMensal;
+float reajuste;
+Console.WriteLine("Digite seu sálario mensal:");
+salarioMensal = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor que deseja o reajuste em porcentagem:");
+reajuste = float.Parse(Console.ReadLine());
+Console.WriteLine($"Digite seu sálario atual é {(reajuste / 100 * salarioMensal) + salarioMensal} com o reajuste de {reajuste / 100 * salarioMensal}");
