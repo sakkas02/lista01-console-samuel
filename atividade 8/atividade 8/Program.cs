05f0d2b534745034578b5cb94be4f0072a857163
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 08:20:27 -0300

    Realizei a atividade 8.

---
+++ atividade 8/atividade 8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite um valor que não seja 0!");
+int num1;
+num1 = int.Parse(Console.ReadLine());
+if (num1 > 0)
+{
+    Console.WriteLine("O número é positivo!");
+}
+else
+{
+    Console.WriteLine("O número é negativo!");
+}
