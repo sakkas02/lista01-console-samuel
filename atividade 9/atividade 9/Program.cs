9e7db2c1ae83184d8bfc399e556382a201df1d3f
Sakkas <bonfanteesamuel@gmail.com>
2025-09-10 08:31:07 -0300

    fiz a atividade 9

---
+++ atividade 9/atividade 9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Escreva quantas maças deseja!");
+int maca;
+
+maca = int.Parse(Console.ReadLine());
+
+if (maca > 12)
+{
+    Console.WriteLine($" O Custo total das {maca} será R${maca}");
+}
+else
+{
+    Console.WriteLine($"O custo total será R${maca*1.3}");
+}
