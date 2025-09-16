02c61f8cc850518ace83bfda744c5b6c20bb7d50
sakkas <bonfanteesamuel@gmail.com>
2025-09-16 09:54:50 -0300

    fiz a atividade 16

---
+++ Atividade 16/Atividade 16/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Digite um valor para N: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 0)
+{
+    Console.WriteLine("Fatorial de número negativo não existe!");
+}
+else
+{
+    int fatorial = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        fatorial *= i;
+    }
+
+    Console.WriteLine($"{n}! = {fatorial}");
+}
