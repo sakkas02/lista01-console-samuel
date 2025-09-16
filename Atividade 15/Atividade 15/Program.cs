49802b3a4aeff7c7de5b8d6578e0f9b6a2c84c9a
sakkas <bonfanteesamuel@gmail.com>
2025-09-16 08:38:26 -0300

    refiz a atividade 14

--- Atividade 15/Atividade 15/Program.cs
+++ Atividade 15/Atividade 15/Program.cs
@@ -1,8 +1,2 @@
-﻿Console.WriteLine("Números entre 1000 e 2000 que deixam resto 5 ao serem divididos por 11:");
-for (int i = 1000; i <= 2000; i++)
-{
-    if (i % 11 == 5)
-    {
-        Console.WriteLine(i);
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
